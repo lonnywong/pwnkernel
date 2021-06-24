22c94429e2d4f6bf1551100c103a7027d4da0406
lonnywong <lonnywong@qq.com>
2021-06-25 00:11:34 +0800

    change home dir

--- launch.sh
+++ launch.sh
@@ -13,7 +13,7 @@ popd
 /usr/bin/qemu-system-x86_64 \
 	-kernel linux-5.4/arch/x86/boot/bzImage \
 	-initrd $PWD/initramfs.cpio.gz \
-	-fsdev local,security_model=passthrough,id=fsdev0,path=$HOME \
+	-fsdev local,security_model=passthrough,id=fsdev0,path=/pwn \
 	-device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare \
 	-nographic \
 	-monitor none \
